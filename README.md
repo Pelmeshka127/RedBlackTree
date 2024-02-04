1b16b1b9e720d791ece7e2b19a814edde3a5bfcd
Kirill Pavlovskiy <kirya0703@gmail.com>
2024-02-04 20:21:07 +0300

    Adding unit-tests, rewriting readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # RedBlackTree
-В данном проекте реализовано красно-черное дерево для решения задачи range queries.
+В данном проекте реализовано красно-черное.
 
 ![image](Images/rbt.jpeg)
+
+# Уровень 1
 
 ## Условие задачи
 Пусть на вход поступают ключи (каждый ключ это целое число, все ключи разные) и запросы (каждый запрос это пара из двух цулых чисел, второе больше первого). Нужно для каждого запроса подсчитать в дереве количество ключей, таких, что все они лежат строго между его левой и правой границами включительно.
@@ -20,38 +22,38 @@ cd RedBlackTree
 ```
 cmake -B build
 
-cd build/src/
+cd build/level1/src/
 
 cmake --build .
 
-./rbt
+./rbt1
 ```
 
 Для запуска unit тестов
 ```
 cmake -B build
 
-cd build/tests/unit
+cd build/level1/tests/unit
 
 cmake --build .
 
-./unit
+./unit1
 ```
 
 Для запуска end to end тестов на дерево и std::set
 ```
 cmake -B build
 
-cd build/tests/ete
+cd build/level1/tests/ete
 
 cmake --build .
 
-./rbtreetest <test> (or) ./stdsettest <test>
+./rbtreetest1 <test> (or) ./stdsettest1 <test>
 ```
 
 Для генерации тестов нужен python3
 ```
-cd tests/ete
+cd level1/tests/ete
 
 python3 testgen.py
 ```
@@ -60,7 +62,7 @@ python3 testgen.py
 ```
 cd build
 
-cd compare
+cd level1/compare
 
 cmake --build .
 
@@ -69,9 +71,64 @@ cmake --build .
 
 Или для уже скомпилированного файла ./comp:
 ```
-cd compare
+cd level1/compare
 
 chmod +x compare.sh
 
 ./compare.sh
+```
+
+# Уровень 2
+
+## Условие задачи
+Со стандартного ввода приходят ключи (каждый ключ это целое число, все ключи разные) и запросы двух видов.
+Запрос (m) на поиск k-го наименьшего элемента. Если запрашивается нулевой (минус первый и т.д.) наименьший выдавайте ошибку и возвращайте ненулевой код возврата. Если запрашивается наименьший элемент с номером больше чем вообще количество элементов в дереве, выдавайте просто последний наименьший (то есть наибольший). Запрос (n) на поиск количества элементов, меньших, чем заданный. Ключи могут быть как угодно перемешаны с запросами. Чтобы успешно пройти тесты, вы должны продумать такую балансировку дерева, чтобы оба вида запросов работали с логарифимической сложностью.
+
+## Установка и запуск проетка
+
+Для установки
+```
+git clone git@github.com:Pelmeshka127/RedBlackTree.git
+
+cd RedBlackTree
+```
+
+Для запуска дерева
+```
+cmake -B build
+
+cd build/level2/src/
+
+cmake --build .
+
+./rbt2
+```
+
+Для запуска unit тестов
+```
+cmake -B build
+
+cd build/level2/tests/unit
+
+cmake --build .
+
+./unit2
+```
+
+Для запуска end to end тестов
+```
+cmake -B build
+
+cd build/level2/tests/ete
+
+cmake --build .
+
+./ete2 <test> 
+```
+
+Для генерации тестов нужен python3
+```
+cd level2/tests/ete
+
+python3 testgen.py
 ```
--- level2/tests/unit/unit.cpp
+++ level2/tests/unit/unit.cpp
@@ -1,9 +1,155 @@
-#include "rbtree.hpp"
-
+#include <iostream>
 #include <gtest/gtest.h>
+#include <fstream>
 #include <gmock/gmock.h>
 
-int main()
+#include "rbtree.hpp"
+
+using namespace std;
+
+//-------------------------------------------------------------------------------//
+
+void EteTestRunner(const char* src, const char* ans)
 {
-    return 0;
+    ifstream test_file{};
+
+    test_file.open(src);
+
+    SearchTree::RBTree<int> tree;
+
+    std::vector<int> test_answers{};
+
+    char cmd{};
+
+    bool mode = true;
+
+    std::clock_t start = clock();
+
+    while(mode && test_file >> cmd)
+    {
+        switch (cmd)
+        {
+            case 'k':
+            {
+                int n = 0;
+
+                test_file >> n;
+
+                tree.Insert(n);
+
+                break;
+            }
+
+            case 'm':
+            {
+                int m = 0;
+
+                test_file >> m;
+
+                test_answers.push_back(tree.MinimumElement(m));
+
+                break;
+            }
+
+            case 'n':
+            {
+                int n = 0;
+
+                test_file >> n;
+
+                test_answers.push_back(static_cast<int>(tree.LessThan(n)));
+
+                break;
+            }
+
+            default:
+            {
+                mode = false;
+            }
+        }
+    }
+
+    test_file.close();
+
+    ifstream ans_file{};
+
+    ans_file.open(ans);
+
+    std::vector<int> answers{};
+
+    while(ans_file.good())
+    {
+        int elem = 0;
+
+        ans_file >> elem;
+
+        answers.push_back(elem);
+    }
+
+    ans_file.close();
+
+    for (int i = 0; i < test_answers.size(); i++)
+    {
+        ASSERT_EQ(test_answers[i], answers[i]);
+    }
 }
+
+//-------------------------------------------------------------------------------//
+
+TEST(EteTest, test1)
+{
+    EteTestRunner("../../../tests/ete/001.dat", "../../../tests/ete/001.answer");
+}
+
+//-------------------------------------------------------------------------------//
+
+TEST(EteTest, test2)
+{
+    EteTestRunner("../../../tests/ete/002.dat", "../../../tests/ete/002.answer");
+}
+
+//-------------------------------------------------------------------------------//
+
+TEST(EteTest, test3)
+{
+    EteTestRunner("../../../tests/ete/003.dat", "../../../tests/ete/003.answer");
+}
+
+//-------------------------------------------------------------------------------//
+
+TEST(EteTest, test4)
+{
+    EteTestRunner("../../../tests/ete/004.dat", "../../../tests/ete/004.answer");
+}
+
+//-------------------------------------------------------------------------------//
+
+TEST(EteTest, test5)
+{
+    EteTestRunner("../../../tests/ete/005.dat", "../../../tests/ete/005.answer");
+}
+
+//-------------------------------------------------------------------------------//
+
+TEST(EteTest, test6)
+{
+    EteTestRunner("../../../tests/ete/006.dat", "../../../tests/ete/006.answer");
+}
+
+//-------------------------------------------------------------------------------//
+
+TEST(EteTest, test7)
+{
+    EteTestRunner("../../../tests/ete/007.dat", "../../../tests/ete/007.answer");
+}
+
+//-------------------------------------------------------------------------------//
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
+//-------------------------------------------------------------------------------//
